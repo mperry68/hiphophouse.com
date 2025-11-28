91543d8964fb6f4bf8109b0a68acbe681095cd38
mperri <marc.perricone@gmail.com>
2025-11-28 17:00:35 -0500

    wip

--- js/main.js
+++ js/main.js
@@ -183,7 +183,7 @@ function initMobileMenu() {
 function getDefaultHeader() {
     return `
         <div class="announcement-bar">
-            <p>Celebrating <span class="highlight">15 years</span> - Thank you for your confidence and trust in us</p>
+            <p>Thank you for your confidence and trust - Celebrating <span class="highlight">15 years</span></p>
         </div>
         <header class="site-header">
             <div class="header-container">
